f133ec01d88220860d36beace2d806b46e66e6e1
Bittmen <38054257+Bittmen@users.noreply.github.com>
2019-11-08 19:27:36 -0600

    form to send project

--- src/components/Modals/modalAdd.js
+++ src/components/Modals/modalAdd.js
@@ -9,25 +9,27 @@ const addProject = (props) => {
                     <Modal.Header closeButton>
                         <Modal.Title>Add new project</Modal.Title>
                     </Modal.Header>
+                    <Form method="POST" action="http://localhost:9000/upload-project" encType="multipart/form-data">
                     <Modal.Body>
                         <Form.Group>
-                            <Form.Control type="text" placeholder="Name"/>
+                            <Form.Control type="text" name="title" placeholder="Name" required />
                         </Form.Group>
                         <Form.Group>
-                            <Form.Control as="textarea" rows="3" placeholder="Description" />
+                            <Form.Control as="textarea" name="content" rows="3" placeholder="Description" required />
                         </Form.Group>
                         <Form.Group>
-                            <Form.Control type="text" placeholder="Url"/>
+                            <Form.Control type="text" name="projectUrl" placeholder="Url" required/>
                         </Form.Group>
                         <Form.Group>
                             <Form.Label>Choose an image for your project</Form.Label>
-                            <Form.Control type="file" placeholder="Image"/>
+                            <Form.Control type="file" name="image" placeholder="Image" required/>
                         </Form.Group>
                     </Modal.Body>
                     <Modal.Footer>
                         <Button variant="secondary" onClick={props.close}>Close</Button>
-                        <Button variant="primary" onClick={props.save}>Save</Button>
+                        <Button type="submit" variant="primary">Save</Button>
                     </Modal.Footer>
+                    </Form>
                 </Modal>
             </>
         )
